80cfba9a06a29e33db1c0ba6438232782d3d7391
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-08 02:59:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,11 +12,11 @@ end
 
 
 def print_first_directors_movie_titles
-  require 'pp'
+  
   movie_index = 0
   
   while movie_index < directors_database[0][:movies].length
-  p directors_database[0][:movies][movie_index][:title]
+  puts directors_database[0][:movies][movie_index][:title]
   
   movie_index += 1
   
